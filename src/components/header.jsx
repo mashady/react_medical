d4211accbebb55658466b2f66beb80b50eb7b580
Mostafa <mostafamokna78@gmail.com>
2025-05-29 18:30:43 +0300

    home

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,20 +4,20 @@ import { AiOutlineCalendar } from 'react-icons/ai';
 const Header = () => {
   return (
     <div className="bg-[#063c37] text-white font-sans">
-      {/* Navbar */}
+     
       <nav className="flex justify-between items-center px-8 py-4 border-b border-[#145F58] relative">
-        {/* Logo */}
+       
         <div className="text-2xl font-bold flex items-center gap-2">
           <span className="text-orange-500 text-3xl">✚</span>
           <span>Medi<span className="text-orange-500">Pro</span></span>
         </div>
 
-        {/* Nav Links */}
+        
         <ul className="flex gap-6 text-white/90 relative">
           <li className="hover:text-orange-400 cursor-pointer text-orange-400">Home</li>
           <li className="hover:text-orange-400 cursor-pointer">About Us</li>
 
-          {/* Services Dropdown */}
+          
           <li className="group relative cursor-pointer">
             <span className="hover:text-orange-400">Services</span>
             <ul className="absolute hidden group-hover:block bg-orange-300 text-black mt-2 py-2 w-40 rounded shadow-lg z-50">
@@ -27,7 +27,7 @@ const Header = () => {
             </ul>
           </li>
 
-          {/* Pages Dropdown */}
+          
           <li className="group relative cursor-pointer">
             <span className="hover:text-orange-400">Pages</span>
             <ul className="absolute hidden group-hover:block bg-orange-300 text-black mt-2 py-2 w-40 rounded shadow-lg z-50">
@@ -40,7 +40,7 @@ const Header = () => {
           <li className="hover:text-orange-400 cursor-pointer">Contact Us</li>
         </ul>
 
-        {/* Book Button */}
+       
         <button className="flex items-center gap-2 bg-transparent border border-orange-400 text-orange-400 px-4 py-2 rounded-full hover:bg-orange-400 hover:text-white transition">
           Book Appointment
           <AiOutlineCalendar size={20} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from '../components/header'
+import HeroSection from '../components/herosection'
+import ContactServiceSection from '../components/serviceinfo'
+import MedicalServicesComponent from '../components/medicalteam'
+import MedicalFooter from '../components/footer'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <HeroSection />
+      <ContactServiceSection />
+      <MedicalServicesComponent />
+      <MedicalFooter />
     </div>
   )
 }
